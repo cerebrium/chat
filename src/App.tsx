ace1726a4d47d1182ddce79868a4a6145993aeb5
Nicholas Shankland <nicholas.m.shankland@gmail.com>
2022-03-05 10:14:33 -0800

    Hook scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useGetData } from "./Requests/hooks/useGetData";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [fetchData, { data, error, loading }] = useGetData();
+
+  useEffect(() => {
+    fetchData("hi/");
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      console.log(error);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
+  return <div className="App">{}</div>;
 }
 
 export default App;
---
+++ src/Requests/hooks/useGetData.ts
@@ -0,0 +1,48 @@
+import React from "react";
+
+type UseGetData = [
+  (url: string) => Promise<void>,
+  {
+    data: any;
+    loading: boolean;
+    error: any;
+  }
+];
+
+const baseUrl = "http://127.0.0.1:3001/";
+
+export const useGetData = (): UseGetData => {
+  const [data, setData] = React.useState<any>(null);
+  const [error, setError] = React.useState<any>(null);
+  const [loading, setLoading] = React.useState(true);
+
+  const getData = async (url = "") => {
+    console.log("address: ", `${baseUrl}${url}`);
+    const response = await fetch(`${baseUrl}${url}`, {
+      method: "GET",
+      // mode: "cors",
+      // cache: "no-cache",
+      // credentials: "same-origin",
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      // redirect: "follow",
+      // referrerPolicy: "no-referrer",
+    });
+    return await response.json();
+  };
+
+  const fetchData = async (url: string) => {
+    try {
+      const data = await getData(url);
+      setData(() => data);
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error);
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  return [fetchData, { data, error, loading }];
+};
